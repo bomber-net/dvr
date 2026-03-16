52eb45de2a074f3099768361ca39765d2fa78621
bomber-net <bomber-net@mail.ru>
2026-03-16 07:25:06 +0300

    wrap DVRWriter main logic into infinite loop

--- app/Services/DVR/DVRWriter.php
+++ app/Services/DVR/DVRWriter.php
@@ -19,24 +19,27 @@ readonly class DVRWriter
 				$outDir=$this->outDir;
 				$socketFile=$this->socketFile;
 				File::ensureDirectoryExists ($outDir);
-				if (file_exists ($socketFile)) unlink ($socketFile);
-				$socket=socket_create (AF_UNIX,SOCK_STREAM,0);
-				socket_bind ($socket,$socketFile,0);
-				socket_listen ($socket);
-				$read=socket_accept ($socket);
-				$acc='';
-				while ($data=socket_read ($read,8192))
+				while (true)
 					{
-						$acc.=$data;
-						if ($pos=strpos ($acc,self::SIGNATURE,self::OFFSET))
+						if (file_exists ($socketFile)) unlink ($socketFile);
+						$socket=socket_create (AF_UNIX,SOCK_STREAM,0);
+						socket_bind ($socket,$socketFile,0);
+						socket_listen ($socket);
+						$read=socket_accept ($socket);
+						$acc='';
+						while ($data=socket_read ($read,8192))
 							{
-								$chunk=substr ($acc,0,$pos);
-								$acc=substr ($acc,$pos);
-								$ts=now ()->format ('Y-m-d H');
-								file_put_contents ("$outDir/{$this->cam->name} $ts.hevc",$chunk,FILE_APPEND);
+								$acc.=$data;
+								if ($pos=strpos ($acc,self::SIGNATURE,self::OFFSET))
+									{
+										$chunk=substr ($acc,0,$pos);
+										$acc=substr ($acc,$pos);
+										$ts=now ()->format ('Y-m-d H');
+										file_put_contents ("$outDir/{$this->cam->name} $ts.hevc",$chunk,FILE_APPEND);
+									}
 							}
+						socket_close ($socket);
+						unlink ($socketFile);
 					}
-				socket_close ($socket);
-				unlink ($socketFile);
 			}
 	}
